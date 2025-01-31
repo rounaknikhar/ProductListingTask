f1148eddc3bf2c40cc3aea7e97c8b0eeec529555
Rounak Nikhar <rounakrezanikhar@hotmail.com>
2025-01-31 20:43:38 +0000

    sort by price fixed, failchecks added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Http\Requests\ProductRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
@@ -16,12 +15,21 @@ class ProductController extends Controller
      */
     public function getProducts(Request $request): JsonResponse | LengthAwarePaginator
     {
-        // Get all products by default sort by price ascending.
-        $products = Product::query()->orderBy('price', 'ASC', true);
+        // Prepare the query to get all products with category.
+        $products = Product::query()->with('category');
 
         // Sort the products.
         if ($request->sort) {
-            $products = $products->orderBy('price', $request->sort, true);
+            // Check if the sort value is valid.
+            if(!in_array($request->sort, ['asc', 'desc'])) {
+                return response()->json(['message' => 'Invalid sort value.'], 400);
+            }
+
+            // Sort the products by price.
+            {{ $request->sort === 'desc' ?  $products = $products->orderByDesc('price') : $products = $products->orderBy('price'); }}
+        } else {
+            // Default sort the products by price in ascending order.
+            $products = $products->orderBy('price');
         }
 
         if ($request->category) {
@@ -41,6 +49,11 @@ class ProductController extends Controller
         if ($request->search) {
             $products = Product::where('name', 'like', '%' . $request->search . '%')
                 ->orWhere('description', 'like', '%' . $request->search . '%');
+        }
+
+        // Check if there are no products.
+        if($products->count() === 0) {
+            return response()->json(['message' => 'No products found.'], 404);
         }
 
         // Paginate the products.
